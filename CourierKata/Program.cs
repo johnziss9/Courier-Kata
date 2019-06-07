f5ef24ae2a27e03153de4d0b272f14412c0e12a2
John Zissimou <jzissimou@travco.co.uk>
2019-06-07 09:56:08 +0100

    Added a weight limit/cost for each parcel size

--- CourierKata/Program.cs
+++ CourierKata/Program.cs
@@ -10,16 +10,22 @@ namespace CourierKata
             Basket basket = new Basket();
 
             bool stillAddingParcels = true;
-            decimal totalPrice = 0;
-            decimal totalShippingPrice = 0;
-            int sParcelCount = 0;
-            int mParcelCount = 0;
-            int lParcelCount = 0;
-            int xlParcelCount = 0;
             decimal sParcelPrice = 0;
             decimal mParcelPrice = 0;
             decimal lParcelPrice = 0;
             decimal xlParcelPrice = 0;
+            decimal totalPrice = 0;
+            decimal totalShippingPrice = 0;
+            decimal weightPrice = 0;
+            decimal sParcelWeight = 1;
+            decimal mParcelWeight = 3;
+            decimal lParcelWeight = 6;
+            decimal xlParcelWeight = 19;
+            decimal parcelWeight = 0;
+            int sParcelCount = 0;
+            int mParcelCount = 0;
+            int lParcelCount = 0;
+            int xlParcelCount = 0;
 
             while (stillAddingParcels)
             {
@@ -38,37 +44,54 @@ namespace CourierKata
                     case "add":
                         {
                             Console.WriteLine("Please enter the number of the parcel you wish to add to the basket.");
-                            Console.WriteLine("1 - Small Parcel (< 10cm), $3");
-                            Console.WriteLine("2 - Medium Parcel (< 50cm), $8");
-                            Console.WriteLine("3 - Large Parcel (< 100cm), $15");
-                            Console.WriteLine("4 - XL Parcel (> 100cm), $25");
+                            Console.WriteLine("1 - Small Parcel (< 10cm, < 1KG), $3");
+                            Console.WriteLine("2 - Medium Parcel (< 50cm, < 3KG), $8");
+                            Console.WriteLine("3 - Large Parcel (< 100cm, < 6KG), $15");
+                            Console.WriteLine("4 - XL Parcel (> 100cm, < 10KG), $25");
+                            Console.WriteLine("An additional $2 will be added for each Kg is the parcel is over the corresponded weight.");
 
                             string parcelInput = Console.ReadLine();
 
                             switch (parcelInput)
                             {
                                 case "1":
+                                    Console.WriteLine("What is the weight of the parcel?");
+                                    parcelWeight = Convert.ToDecimal(Console.ReadLine());
+                                    weightPrice += parcelWeight <= 1 ? weightPrice = 0 : weightPrice = (parcelWeight - 1) * 2;
                                     Console.WriteLine("Parcel Added");
                                     basket.AddToBasket("Small Parcel", 3);
                                     sParcelCount++;
+                                    basket.AddToBasket("Additional Weight Cost", weightPrice);
                                     totalShippingPrice += SpeedyShipping(parcelInput, 3, totalShippingPrice, basket);
                                     break;
                                 case "2":
+                                    Console.WriteLine("What is the weight of the parcel?");
+                                    parcelWeight = Convert.ToDecimal(Console.ReadLine());
+                                    weightPrice += parcelWeight <= 3 ? weightPrice = 0 : weightPrice = (parcelWeight - 1) * 2;
                                     Console.WriteLine("Parcel Added");
                                     basket.AddToBasket("Medium Parcel", 8);
                                     sParcelCount++;
+                                    basket.AddToBasket("Additional Weight Cost", weightPrice);
                                     totalShippingPrice += SpeedyShipping(parcelInput, 8, totalShippingPrice, basket);
                                     break;
                                 case "3":
+                                    Console.WriteLine("What is the weight of the parcel?");
+                                    parcelWeight = Convert.ToDecimal(Console.ReadLine());
+                                    weightPrice += parcelWeight <= 6 ? weightPrice = 0 : weightPrice = (parcelWeight - 1) * 2;
                                     Console.WriteLine("Parcel Added");
                                     basket.AddToBasket("Large Parcel", 15);
                                     sParcelCount++;
+                                    basket.AddToBasket("Additional Weight Cost", weightPrice);
                                     totalShippingPrice += SpeedyShipping(parcelInput, 15, totalShippingPrice, basket);
                                     break;
                                 case "4":
+                                    Console.WriteLine("What is the weight of the parcel?");
+                                    parcelWeight = Convert.ToDecimal(Console.ReadLine());
+                                    weightPrice += parcelWeight <= 10 ? weightPrice = 0 : weightPrice = (parcelWeight - 1) * 2;
                                     Console.WriteLine("Parcel Added");
                                     basket.AddToBasket("XL Parcel", 25);
                                     sParcelCount++;
+                                    basket.AddToBasket("Additional Weight Cost", weightPrice);
                                     totalShippingPrice += SpeedyShipping(parcelInput, 25, totalShippingPrice, basket);
                                     break;
                                 default:
@@ -88,7 +111,7 @@ namespace CourierKata
                             lParcelPrice = lParcelCount * 15;
                             xlParcelPrice = xlParcelCount * 25;
 
-                            totalPrice = sParcelPrice + mParcelPrice + lParcelPrice + xlParcelPrice + totalShippingPrice;
+                            totalPrice = sParcelPrice + mParcelPrice + lParcelPrice + xlParcelPrice + totalShippingPrice + weightPrice;
 
                             if (totalPrice != 0)
                             {
@@ -152,6 +175,16 @@ namespace CourierKata
             }
 
             return totalShippingPrice;
+        }
+
+        public static int ParcelWeight(string parcelInput)
+        {
+            Console.WriteLine("What is the weight of the parcel?");
+            int parcelWeight = Convert.ToInt32(Console.ReadLine());
+
+
+
+            return parcelWeight;
         }
     }
 }
